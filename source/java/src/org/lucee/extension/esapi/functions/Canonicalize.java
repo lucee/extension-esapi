3403920762be2144906ba49336b3eae11de405ce
Zac Spitzer <zac.spitzer@gmail.com>
2023-09-08 10:24:20 +0200

    LDEV-4687 change Canonicalize and DecodeFromURL to extend FunctionSupport
    https://luceeserver.atlassian.net/browse/LDEV-4687

--- source/java/src/org/lucee/extension/esapi/functions/Canonicalize.java
+++ source/java/src/org/lucee/extension/esapi/functions/Canonicalize.java
@@ -19,44 +19,57 @@
 package org.lucee.extension.esapi.functions;
 
 import lucee.runtime.PageContext;
-import lucee.runtime.ext.function.Function;
+import lucee.runtime.exp.PageException;
 import java.net.URLDecoder;
 
 
-public class Canonicalize implements Function {
+public class Canonicalize extends FunctionSupport {
 
 	private static final long serialVersionUID = -4248746351014698481L;
 
-	public static String call(PageContext pc,String input, boolean restrictMultiple, boolean restrictMixed) throws
-		Exception{
-	
-		return ESAPIEncode.canonicalize(input, restrictMultiple, restrictMixed, false);
+	public static String call(PageContext pc,String input, boolean restrictMultiple, boolean restrictMixed) throws PageException{
+		try {
+			return ESAPIEncode.canonicalize(input, restrictMultiple, restrictMixed, false);
+		} 
+		catch (Exception ee) {
+			throw cast.toPageException(ee);
+		}
 	}
 
-	public static String call(PageContext pc,String input, boolean restrictMultiple, boolean restrictMixed, boolean throwonError) throws Exception{
+	public static String call(PageContext pc,String input, boolean restrictMultiple, boolean restrictMixed, boolean throwonError) throws PageException{
 		
 		String str = input;
 		String decodeUrl = " ";
-        String strFind = "%";
-        int count = 0, fromIndex = 0;
-        
-        while ((fromIndex = str.indexOf(strFind, fromIndex)) != -1 ){
-            count++;
-            fromIndex++;
-        }
+		String strFind = "%";
+		int count = 0, fromIndex = 0;
+		
+		while ((fromIndex = str.indexOf(strFind, fromIndex)) != -1 ){
+			count++;
+			fromIndex++;
+		}
 
-		decodeUrl = URLDecoder.decode(input, "UTF-8");
-
-		if(decodeUrl == input) return ESAPIEncode.canonicalize(input, restrictMultiple, restrictMixed, throwonError);
-			
-		else {
-			if(throwonError == false && (restrictMultiple == true || restrictMixed == true)) {
-				if(count > 0) return " ";
-				else return ESAPIEncode.canonicalize(input, restrictMultiple, restrictMixed, throwonError);
-			}
+		try {
+			decodeUrl = URLDecoder.decode(input, "UTF-8");
+			if(decodeUrl == input) return ESAPIEncode.canonicalize(input, restrictMultiple, restrictMixed, throwonError);
 			else {
-				return ESAPIEncode.canonicalize(input, restrictMultiple, restrictMixed, throwonError);
+				if(throwonError == false && (restrictMultiple == true || restrictMixed == true)) {
+					if(count > 0) return " ";
+					else return ESAPIEncode.canonicalize(input, restrictMultiple, restrictMixed, throwonError);
+				}
+				else {
+					return ESAPIEncode.canonicalize(input, restrictMultiple, restrictMixed, throwonError);
+				}
 			}
 		}
+		catch (Exception ee) {
+			throw cast.toPageException(ee);
+		}
+	}
+
+	@Override
+	public Object invoke(PageContext pc, Object[] args) throws PageException {
+		if(args.length==3) return call(pc,cast.toString(args[0]), cast.toBoolean(args[1]), cast.toBoolean(args[2]));
+		if(args.length==4) return call(pc,cast.toString(args[0]), cast.toBoolean(args[1]), cast.toBoolean(args[2]), cast.toBoolean(args[3]));
+		throw exp.createFunctionException(pc, "Canonicalize", 3, 4, args.length);
 	}
 }
