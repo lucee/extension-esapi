978e97733607604b1d1aff346cbc542185415ced
Michael Offner <michael@lucee.org>
2026-01-07 10:10:58 +0100

    make encode and decode function available (used by the core)

---
+++ source/java/src/org/lucee/extension/esapi/functions/ESAPIDecode.java
@@ -0,0 +1,8 @@
+package org.lucee.extension.esapi.functions;
+
+import org.lucee.extension.guard.functions.GuardDecode;
+
+@Deprecated
+public class ESAPIDecode extends GuardDecode {
+
+}
---
+++ source/java/src/org/lucee/extension/esapi/functions/ESAPIEncode.java
@@ -0,0 +1,8 @@
+package org.lucee.extension.esapi.functions;
+
+import org.lucee.extension.guard.functions.GuardEncode;
+
+@Deprecated
+public class ESAPIEncode extends GuardEncode {
+
+}
